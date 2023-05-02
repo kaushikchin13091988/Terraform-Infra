16533471b22f09a7d77c0ebf753105de752e721c
Kaushik <kaushikchin@gmail.com>
2023-05-02 14:06:39 +0530

    added code build

--- Terraform/modules/cicd/main.tf
+++ Terraform/modules/cicd/main.tf
@@ -1,14 +1,12 @@
 resource "aws_codebuild_project" "ProductServiceCodeBuild" {
     badge_enabled          = false
     build_timeout          = 60
-    concurrent_build_limit = 0
-    encryption_key         = "arn:aws:kms:us-east-1:385501908346:alias/aws/s3"
-    id                     = "products-service-build"
-    name                   = "products-service-build"
+    concurrent_build_limit = 1
+    name                   = "product-service-build"
     project_visibility     = "PRIVATE"
     queued_timeout         = 480
-    service_role           = "arn:aws:iam::385501908346:role/service-role/codebuild-products-service-build-service-role"
-    source_version         = "refs/heads/master"
+    service_role           = aws_iam_role.CodeBuildRole.arn
+    source_version         = "refs/heads/main"
     tags                   = {}
     tags_all               = {}
 
@@ -26,7 +24,6 @@ resource "aws_codebuild_project" "ProductServiceCodeBuild" {
     environment {
         compute_type                = "BUILD_GENERAL1_SMALL"
         image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
-        image_pull_credentials_type = "CODEBUILD"
         privileged_mode             = true
         type                        = "LINUX_CONTAINER"
 
@@ -54,122 +51,128 @@ resource "aws_codebuild_project" "ProductServiceCodeBuild" {
 
     logs_config {
         cloudwatch_logs {
-            group_name  = "products-service-log-group"
+            group_name  = "product-service-code-build-log-group"
             status      = "ENABLED"
-            stream_name = "products-service-log-stream"
-        }
-
-        s3_logs {
-            encryption_disabled = false
-            status              = "DISABLED"
         }
     }
 
     source {
         git_clone_depth     = 1
         insecure_ssl        = false
-        location            = "https://git-codecommit.us-east-1.amazonaws.com/v1/repos/products-service"
+        location            = "https://github.com/kaushikchin13091988/Sample-Microservice.git"
         report_build_status = false
-        type                = "CODECOMMIT"
+        type                = "GITHUB"
 
         git_submodules_config {
-            fetch_submodules = false
+            fetch_submodules = true
         }
     }
 }
 
-# aws_codecommit_repository.productServiceCodeCommit:
-resource "aws_codecommit_repository" "ProductServiceCodeCommit" {
-    clone_url_http  = "https://git-codecommit.us-east-1.amazonaws.com/v1/repos/products-service"
-    clone_url_ssh   = "ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/products-service"
-    id              = "products-service"
-    repository_id   = "60aaae7e-1b6e-4830-a136-eb85172752fc"
-    repository_name = "products-service"
-    tags            = {}
-    tags_all        = {}
+resource "aws_codebuild_source_credential" "ProductServiceCodeBuildSourceCredential" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = "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"
 }
 
-# aws_codepipeline.productServiceCodePipeline:
-resource "aws_codepipeline" "ProductServiceCodePipeline" {
-    id       = "products-service-pipeline"
-    name     = "products-service-pipeline"
-    role_arn = "arn:aws:iam::385501908346:role/service-role/AWSCodePipelineServiceRole-us-east-1-catpipeline"
-    tags     = {}
-    tags_all = {}
+resource "aws_iam_role" "CodeBuildRole" {
+  name = "CodeBuildRole"
 
-    artifact_store {
-        location = "codepipeline-us-east-1-743172729228"
-        type     = "S3"
-    }
-
-    stage {
-        name = "Source"
-
-        action {
-            category         = "Source"
-            configuration    = {
-                "BranchName"           = "master"
-                "OutputArtifactFormat" = "CODE_ZIP"
-                "PollForSourceChanges" = "false"
-                "RepositoryName"       = "products-service"
+  assume_role_policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "codebuild.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
             }
-            input_artifacts  = []
-            name             = "Source"
-            namespace        = "SourceVariables"
-            output_artifacts = [
-                "SourceArtifact",
-            ]
-            owner            = "AWS"
-            provider         = "CodeCommit"
-            region           = "us-east-1"
-            run_order        = 1
-            version          = "1"
-        }
-    }
-    stage {
-        name = "Build"
-
-        action {
-            category         = "Build"
-            configuration    = {
-                "ProjectName" = "products-service-build"
-            }
-            input_artifacts  = [
-                "SourceArtifact",
-            ]
-            name             = "Build"
-            namespace        = "BuildVariables"
-            output_artifacts = [
-                "BuildArtifact",
-            ]
-            owner            = "AWS"
-            provider         = "CodeBuild"
-            region           = "us-east-1"
-            run_order        = 1
-            version          = "1"
-        }
-    }
-    stage {
-        name = "Deploy"
-
-        action {
-            category         = "Deploy"
-            configuration    = {
-                "ClusterName" = "products-service-ecs-cluster"
-                "FileName"    = "imagedefinitions.json"
-                "ServiceName" = "products-ecs-service"
-            }
-            input_artifacts  = [
-                "BuildArtifact",
-            ]
-            name             = "Deploy"
-            namespace        = "DeployVariables"
-            output_artifacts = []
-            owner            = "AWS"
-            provider         = "ECS"
-            region           = "us-east-1"
-            run_order        = 1
-            version          = "1"
-        }
-    }
+        ]
+    })
 }
+
+resource "aws_iam_policy" "CodeBuildBasePolicy" {
+  name = "CodeBuildBasePolicy"
+  policy = file("modules/cicd/CodeBuildBasePolicy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "CodeBuildBasePolicyAttachment" {
+  role       = aws_iam_role.CodeBuildRole.name
+  policy_arn = aws_iam_policy.CodeBuildBasePolicy.arn
+}
+
+# resource "aws_codepipeline" "CodePipelinePipeline" {
+#     name = "product-service-pipeline"
+#     role_arn = "arn:aws:iam::385501908346:role/service-role/CodePipelineRole"
+#     artifact_store {
+#         location = "codepipeline-us-east-1-892006371696"
+#         type = "S3"
+#     }
+#     stages {
+#         name = "Source"
+#         action = [
+#             {
+#                 name = "Source"
+#                 category = "Source"
+#                 owner = "AWS"
+#                 configuration {
+#                     BranchName = "main"
+#                     ConnectionArn = "arn:aws:codestar-connections:us-east-2:385501908346:connection/35407294-64f9-4fa3-b806-e1b0fa8e7474"
+#                     DetectChanges = "true"
+#                     FullRepositoryId = "kaushikchin13091988/Sample-Microservice"
+#                     OutputArtifactFormat = "CODE_ZIP"
+#                 }
+#                 provider = "CodeStarSourceConnection"
+#                 version = "1"
+#                 output_artifacts = [
+#                     "SourceArtifact"
+#                 ]
+#                 run_order = 1
+#             }
+#         ]
+#     }
+#     stages {
+#         name = "Build"
+#         action = [
+#             {
+#                 name = "Build"
+#                 category = "Build"
+#                 owner = "AWS"
+#                 configuration {
+#                     ProjectName = "product-service-build"
+#                 }
+#                 input_artifacts = [
+#                     "SourceArtifact"
+#                 ]
+#                 provider = "CodeBuild"
+#                 version = "1"
+#                 output_artifacts = [
+#                     "BuildArtifact"
+#                 ]
+#                 run_order = 1
+#             }
+#         ]
+#     }
+#     stages {
+#         name = "Deploy"
+#         action = [
+#             {
+#                 name = "Deploy"
+#                 category = "Deploy"
+#                 owner = "AWS"
+#                 configuration {
+#                     ClusterName = "test-ecs-cluster"
+#                     FileName = "imagedefinitions.json"
+#                     ServiceName = "products-ecs-service"
+#                 }
+#                 input_artifacts = [
+#                     "BuildArtifact"
+#                 ]
+#                 provider = "ECS"
+#                 version = "1"
+#                 run_order = 1
+#             }
+#         ]
+#     }
+# }
